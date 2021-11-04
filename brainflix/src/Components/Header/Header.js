d338ceaac3ac4dcb8bc4fe50dca56667ec7f3e2c
MarkMarley-Dev <markmarley19911@gmail.com>
2021-11-04 16:25:32 +0000

    Updated file structure work on header

---
+++ brainflix/src/Components/Header/Header.js
@@ -0,0 +1,23 @@
+import "../Header/header.scss";
+export default function Header() {
+  /* props;  add props in later*/
+
+  return (
+    <header className="header">
+      <div className="header__Logo"></div>
+      <div className="header__middle-mobile">
+        <input
+          className="header__search-box"
+          type="search"
+          placeholder="Search"
+        />
+        <div className="header__img"></div>
+      </div>
+      <div>
+        <button type="submit" class="header__upload-btn">
+          Upload
+        </button>
+      </div>
+    </header>
+  );
+}
